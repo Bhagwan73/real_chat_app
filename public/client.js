7e84111b44949e15107a78375e97cc6029523827
Bhagwan73 <bhagwan735025@gmail.com>
2022-12-19 10:28:11 +0530

    chat_app

--- public/client.js
+++ public/client.js
@@ -1,7 +1,8 @@
 const socket = io()
 let name;
 let textarea = document.querySelector('#textarea')
-let messageArea = document.querySelector('.message__area')
+let messageArea = document.querySelector('.chat__section')
+
 do {
     name = prompt('Please enter your name: ')
 } while(!name)
@@ -31,14 +32,20 @@ function appendMessage(msg, type) {
     let mainDiv = document.createElement('div')
     let className = type
     mainDiv.classList.add(className, 'message')
-
+    
+    
     let markup = `
-        <h4>${msg.user}</h4>
+        
         <p>${msg.message}</p>
+        <h4> --${msg.user}</h4>
+
     `
     mainDiv.innerHTML = markup
     messageArea.appendChild(mainDiv)
 }
+
+
+
 
 // Recieve messages 
 socket.on('message', (msg) => {
@@ -49,6 +56,4 @@ socket.on('message', (msg) => {
 function scrollToBottom() {
     messageArea.scrollTop = messageArea.scrollHeight
 }
-
-
 
